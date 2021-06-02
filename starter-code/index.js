6665625edf97a5ed108ffcb6c1e2399dc8278e63
jordiglomer <glomerfil@gmail.com>
2021-06-02 09:05:03 +0200

    casa2 no funciona, no error

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,60 +1,66 @@
-//ADD ITEM
-/*window.addEventListener('load', () => {
+window.addEventListener('load', () => {
+
     const input = document.querySelector('input');
     const button = document.addEventListener('click', addItem);
+    const lista = document.querySelector('.list');
+    //const button = document.querySelector("button").addEventListener("click", addItem);
 
 });
 
-
 let addItem = () => {
-    let input = document.querySelector('input'),
-        if(value.length = 0){ 
-            newItem(input.value);
-        } 
-};
-let newItem = (nameItem) => {
-    let checkItem = document.createElement('a');
-    let deleteItem = document.createElement('a');
-    let text = document.createTextNode(newItem);
-    let item = document.createElement('li');
-    item.appendChild(text);
-    item.appendChild(checkItem);
-    item.appendChild(deleteItem);
 
-    deleteItem.addEventListener('click', removeItem);
-    checkItem.addEventListener('click', checkedItem);
+    //let currentItem = document.getElementsByTagName('input');
+
+    let newItem;
+    let input = document.querySelector('input');
+    if (input.value.length > 0) {
+        newItem = (input.value);
+        input.value = '';
+    }
+
+
+    let listItem = document.createElement('li');
+    let checkItem = document.createElement('a');
+    let text = document.createTextNode(nombreItem);
+    let deleteItem = document.createElement('a');
+
+    listItem.classList.add('.list__item');
+    deleteItem.classList.add('.list__delete-btn');
+    checkItem.classList.add('.list__check-btn');
+
+    listItem.appendChild(checkItem);
+    listItem.appendChild(deleteItem);
+
+    let text = document.createTextNode(newItem);
+    listItem.appendChild(text);
 
     deleteItem.innerText = 'x';
     checkItem.innerText = '✔';
 
-    item.classList.add("list__item");
-    deleteItem.classList.add("list__delete-btn");
-    checkItem.classList.add("list__check-btn");
-    document.querySelector("ul").appendChild(item);
+    listItem.appendChild('.list');
 
-
-}
-
-
-//LIST ITEMS 
-const listItems = document.getElementsByClassName('.list');
-
-button.addEventListener('click', () => {
-    let list = document.querySelector('.list__item');
-
-    let li = document.createElement('li');
-    li.textContent = input.value;
-    let appendedItem = list.appendChild(li);
-
-    for (let i = 0; i < appendedItem.length; i++) {
-        appendedItem[i].style.color = lastPickedColor;
-    }
-    input.value = '';
-});*/
-
-
-window.onload = function() {
-    Infinity();
-    const input = document.querySelector('input');
-    const button = document.addEventListener('click', addItem);
 };
+//checkItem.addEventListener('click', checkedItem);
+//document.querySelector("ul").appendChild(item);
+
+let removeItem = (deleteItem) => {
+    deleteItem.currentTarget.parentNode.remove();
+    input.item(0).focus();
+    deleteItem.addEventListener('click', removeItem);
+};
+
+let checkedItem = (checkItem) => {
+    let parent_li_anchor = checkItem.currentTarget.parentNode;
+    if (parent_li_anchor.classList.contains(class_item_cheked)) {
+        parent_li_anchor.classList.remove(class_item_cheked);
+    } else {
+        parent_li_anchor.classList.add(class_item_cheked);
+    }
+
+
+    i_inputs.item(0).focus();
+
+
+};
+
+//window.onload = init();
