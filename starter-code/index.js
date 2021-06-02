d3a7e7170b9ddd268e63db60e84ff86f78f3d6b7
jordiglomer <glomerfil@gmail.com>
2021-06-02 21:37:00 +0200

    algo mes

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,66 +1,68 @@
 window.addEventListener('load', () => {
 
-    const input = document.querySelector('input');
-    const button = document.addEventListener('click', addItem);
-    const lista = document.querySelector('.list');
-    //const button = document.querySelector("button").addEventListener("click", addItem);
+    //newItem("Milk");
+
+    let button = document.addEventListener('click', addItem);
+    let input = document.querySelector('input');
+    let lista = document.querySelector('.list');
+    //let input = document.querySelector('button').addEventListener('click', addItem);
+    //button.querySelector("button");
 
 });
 
 let addItem = () => {
 
     //let currentItem = document.getElementsByTagName('input');
-
-    let newItem;
+    //let newItem;
     let input = document.querySelector('input');
     if (input.value.length > 0) {
         newItem = (input.value);
         input.value = '';
     }
-
+};
+let newItem = (item) => {
 
     let listItem = document.createElement('li');
     let checkItem = document.createElement('a');
-    let text = document.createTextNode(nombreItem);
+    let text = document.createTextNode(item);
     let deleteItem = document.createElement('a');
 
-    listItem.classList.add('.list__item');
-    deleteItem.classList.add('.list__delete-btn');
-    checkItem.classList.add('.list__check-btn');
-
     listItem.appendChild(checkItem);
-    listItem.appendChild(deleteItem);
-
-    let text = document.createTextNode(newItem);
     listItem.appendChild(text);
+    listItem.appendChild(deleteItem);
 
     deleteItem.innerText = 'x';
     checkItem.innerText = '✔';
 
-    listItem.appendChild('.list');
+    listItem.classList.add('.list__item');
+    deleteItem.classList.add('.list__delete-btn');
+    checkItem.classList.add('.list__check-btn');
+    document.querySelector('ul').appendChild(listItem);
 
 };
-//checkItem.addEventListener('click', checkedItem);
-//document.querySelector("ul").appendChild(item);
+
+
 
 let removeItem = (deleteItem) => {
-    deleteItem.currentTarget.parentNode.remove();
+    //deleteItem.currentTarget.parentNode.remove();
     input.item(0).focus();
     deleteItem.addEventListener('click', removeItem);
 };
 
 let checkedItem = (checkItem) => {
-    let parent_li_anchor = checkItem.currentTarget.parentNode;
+    //checkItem.currentTarget.parentNode.add();
+    /*let parent_li_anchor = checkItem.currentTarget.parentNode;
     if (parent_li_anchor.classList.contains(class_item_cheked)) {
         parent_li_anchor.classList.remove(class_item_cheked);
     } else {
         parent_li_anchor.classList.add(class_item_cheked);
-    }
+    }*/
 
+    input.item(0).focus();
+    checkItem.addEventListener('click', checkedItem);
 
-    i_inputs.item(0).focus();
 
 
 };
 
-//window.onload = init();
+window.onload = init; // Parece que sin esta linea no se puede debugar con el inspector del navegador.
